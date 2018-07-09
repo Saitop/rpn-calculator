02e81de406f7e52e25a60cf8acc8930c5401bf17
Hanxian Lin <saitoplam@hotmail.com>
2018-07-09 16:38:59 +0800

    show input

--- src/main/java/com/airwallex/RpnCalculator.java
+++ src/main/java/com/airwallex/RpnCalculator.java
@@ -1,8 +1,11 @@
 package com.airwallex;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class RpnCalculator {
+
+    private static final String EXIT_CODE = ":q";
 
     public static void main(String[] args) {
         final RpnCalculator rpnCalculator = new RpnCalculator();
@@ -10,29 +13,30 @@ public class RpnCalculator {
     }
 
     private void run() {
-        construction();
+        instruction();
         Scanner scan = new Scanner(System.in);
+
         while(scan.hasNextLine()) {
             String input = scan.nextLine();
             exit(input);
             System.out.println("===================================");
             System.out.println("Result as below:");
+            System.out.println("-----------------------------------");
             //todo: make it real
-            System.out.printf("%s \n", input);
-            System.out.println("===================================");
+            System.out.printf("Input:\t%s\t\n", input);
 
         }
 
     }
 
     private void exit(String input) {
-        if (":q".equals(input)) {
+        if (EXIT_CODE.equals(input)) {
             System.out.println("Exit!");
             System.exit(0);
         }
     }
 
-    private void construction() {
+    private void instruction() {
         System.out.println("*******************************************************");
         System.out.println("Welcome to RPN Calculator!");
         System.out.println("Enter ':q' to exit the program");
