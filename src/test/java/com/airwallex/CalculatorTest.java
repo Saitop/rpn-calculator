453e1c60328ac2c1916f61337dd0bf0ad50c6551
Hanxian Lin <saitoplam@hotmail.com>
2018-07-14 17:48:17 +0800

    implement undo operation

--- src/test/java/com/airwallex/CalculatorTest.java
+++ src/test/java/com/airwallex/CalculatorTest.java
@@ -30,8 +30,38 @@ public class CalculatorTest {
     @Test
     public void shouldReturnSquareRootWithClearToken() {
         Calculator calculator = new Calculator();
-        calculator.process("2 sqrt clear 9 sqrt");
+        calculator.process("2 sqrt");
+        assertEquals("1.4142135623", calculator.printNumberStack());
+
+        calculator.process("clear 9 sqrt");
         assertEquals("3", calculator.printNumberStack());
+    }
+
+    @Test
+    public void shouldReturnCorrectValueWhenInputMultipleOperator() {
+        Calculator calculator = new Calculator();
+        calculator.process("5 2 -");
+        assertEquals("3", calculator.printNumberStack());
+
+        calculator.process("3 -");
+        assertEquals("0", calculator.printNumberStack());
+
+        calculator.process("clear");
+        assertEquals("", calculator.printNumberStack());
+
+    }
+
+    @Test
+    public void shouldReturnCorrectValueWhitUndoOperator() {
+        Calculator calculator = new Calculator();
+        calculator.process("5 4 3 2");
+        assertEquals("5 4 3 2", calculator.printNumberStack());
+
+        calculator.process("undo undo * ");
+        assertEquals("20", calculator.printNumberStack());
+
+        calculator.process("5 * ");
+        assertEquals("100", calculator.printNumberStack());
     }
 
     @Test
